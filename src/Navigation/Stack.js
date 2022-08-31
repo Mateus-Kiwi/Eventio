be5bdb32da7ea8cf0f45ddce57cf6da494afcc0f
DESKTOP-DCG8JEE\LopesZ <joao_vm_lopes@estudante.sc.senai.br>
2022-08-30 21:20:29 -0300

    LoginToHome Pt2 :l

---
+++ src/Navigation/Stack.js
@@ -0,0 +1,22 @@
+
+
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
+
+const Stack = createStackNavigator()
+
+import Login from './screens/Login';
+import Home from './Main';
+
+export default function App() {
+  return (
+ <NavigationContainer independent={true}>
+   <Stack.Navigator LoginRouteName='Login'>
+     <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+     <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+   </Stack.Navigator>
+ </NavigationContainer>
+  );
+}
